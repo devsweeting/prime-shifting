dab1a2dfc848bb2ec54d3cba08317ca4a29c702f
entegral <e2robby@gmail.com>
2018-11-15 08:14:45 -0800

    change function to class

--- lib/example.rb
+++ lib/example.rb
@@ -1,8 +1,15 @@
-def example(title)
-  # this is an example function. It has a spec associated with it in the 'spec' directory
-  split_sentence = title.split
-  split_sentence.each do |word|
-    word.capitalize!()
+class Cat
+  attr_reader(:name, :breed)
+  attr_writer(:address)
+  attr_accessor(:age)
+
+  def initialize(name, age, breed, address)
+    @name = name
+    @age = age
+    @breed = breed
+    @birthplace = birthplace
   end
-  split_sentence.join(' ')
-end
+
+  def meow
+    puts "meow"
+  end
--- spec/example_spec.rb
+++ spec/example_spec.rb
@@ -2,14 +2,9 @@ require ('rspec')
 require('example')
 require ('pry')
 
-describe('#example') do
-  # eventually put specs here
-  it("capitalizes the first letter of a word") do
-    expect(title_case("beowulf")).to(eq("Beowulf"))
-  end
-
-  it('capitalizes the first letter of all words in a multibple word title') do
-    expect(title_case('the color purple')).to(eq('The Color Purple'))
+describe('#cat') do
+  it("#makes the cat meow") do
+    expect(meow()).to(eq("meow"))
   end
 
 end
