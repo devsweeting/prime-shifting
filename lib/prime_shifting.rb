2ff0715644be9fd6e1a15a8ff77180ca7e3aaa5f
Devin Sweeting and Ralph Perdomo <ralph@>
2018-11-15 13:47:59 -0800

    hard code primes

--- lib/prime_shifting.rb
+++ lib/prime_shifting.rb
@@ -1,9 +1,58 @@
 require ('pry')
 
-  def prime_shift (input)
-    array = (2..(input)).to_a()
-    array.delete_if do |array_element|
-      array_element % 2 == 0
+def prime_shift (input)
+  prime = 2
+  prime_array = []
+  array = (prime..(input)).to_a()
+
+  array.delete_if do |input|
+    if input > 2
+      input % 2 == 0
+    end
   end
-  array
+
+  array.delete_if do |input|
+    if input > 3
+      input % 3 == 0
+    end
+  end
+
+  array.delete_if do |input|
+    if input > 5
+      input % 5 == 0
+    end
+  end
+end
+
+  array.delete_if do |input|
+    if input > 7
+      input % 7 == 0
+    end
+  end
+end
+
+  # captured_length = array.length()
+  #
+  # while (prime < input )
+  #
+  #   array.each do |element|
+  #
+  #     if (element % prime == 0)
+  #       prime_array.push(element)
+  #     end
+  #   prime += 1
+  #   # binding.pry
+  #
+  #   end
+
+  end
+
+
+  # captured_length.times() do |i|
+  #   prime += i
+  #   array.delete_if do |element|
+  #     element % prime == 0
+  #   end
+  # end
+  prime_array
 end
--- spec/prime_shifting_spec.rb
+++ spec/prime_shifting_spec.rb
@@ -1,8 +1,11 @@
 require 'rspec'
 require 'prime_shifting'
 
-describe("prime_shift") do
-  # it("Will remove all divisibles of 2") do
-  #   expect(prime_shift(10)).to(eq([3,5,7,9]))
-  # end
+describe("something") do
+  it ("Will delete divisbles by 2") do
+    expect(prime_shift(10)).to(eq([3,5,7,9]))
+  end
+  it ("Will delete divisbles of 3") do
+    expect(prime_shift(69)).to(eq([5,7,]))
+  end
 end
